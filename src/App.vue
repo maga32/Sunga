cbcfe852b8a3e2b69d8564d5110bb8b475c7baf8
seunghwa <maga32@naver.com>
2023-05-15 05:43:53 +0000

    230515-1 메타태그 수정 / 파비콘 수정 / 네비게이션바 상단고정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <nav class="sticky-top bg-white">
+    <router-link to="/">Home</router-link>&nbsp;&nbsp;|&nbsp;
     <router-link to="/project">Project</router-link>
   </nav>
   <router-view/>
